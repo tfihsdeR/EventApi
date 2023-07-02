2c9f16de00d563a33b8d19adf6e4c627e2f88bda
tfihsdeR <erdincatay@yandex.com>
2023-07-02 23:03:32 +0300

    Update_Context

--- Data/Repository/AppDbContext.cs
+++ Data/Repository/AppDbContext.cs
@@ -14,11 +14,6 @@ namespace EventApi.Data.Repository
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
-			//modelBuilder.Entity<Event>().HasOne(e => e.Category).WithMany(c => c.Events).HasForeignKey(e => e.CategoryId);
-			//modelBuilder.Entity<Event>().HasOne(e => e.Venue).WithMany(v => v.Events).HasForeignKey(e => e.VenueId);
-			//modelBuilder.Entity<Event>().HasOne(e => e.PriceBySeat).WithMany(pbs => pbs.Events).HasForeignKey(e => e.PriceBySeatId);
-			//modelBuilder.Entity<Image>().HasOne(i => i.Event).WithMany(e => e.Images).HasForeignKey(i => i.EventId);
-
 			modelBuilder.Entity<Category>().HasData(new Category()
 			{
 				Id = 1,
@@ -93,7 +88,7 @@ namespace EventApi.Data.Repository
 			new Event()
 			{
 				Id = 1,
-				Name = "Tarkan Konseri",
+				Name = "Tarkan Concert",
                 Organizor = "Tarkan",
 				StartDate = new DateTime(2023, 7, 13),
 				EndDate = new DateTime(2023, 7, 15),
@@ -103,7 +98,7 @@ namespace EventApi.Data.Repository
 			new Event()
 			{
 				Id = 2,
-				Name = "Jason Derulo Konseri",
+				Name = "Jason Derulo Concert",
                 Organizor = "Jason Derulo",
                 StartDate = new DateTime(2023, 7, 16),
 				EndDate = new DateTime(2023, 7, 19),
@@ -113,7 +108,7 @@ namespace EventApi.Data.Repository
 			new Event()
 			{
 				Id = 3,
-				Name = "Şebnem Ferah Konseri",
+				Name = "Şebnem Ferah Concert",
                 Organizor = "Şebnem Ferah",
                 StartDate = new DateTime(2023, 9, 10),
 				EndDate = new DateTime(2023, 9, 15),
@@ -124,7 +119,7 @@ namespace EventApi.Data.Repository
 			new Event()
 			{
 				Id = 4,
-				Name = "Leonardo da Vinci Resim Sergisi",
+				Name = "Leonardo da Vinci Art Galery",
                 Organizor = "Topyekün Art Galery",
 				StartDate = new DateTime(2023, 8, 21),
 				EndDate = new DateTime(2023, 8, 30),
@@ -134,7 +129,7 @@ namespace EventApi.Data.Repository
             new Event()
             {
                 Id = 5,
-                Name = "Naruto Resim Sergisi",
+                Name = "Naruto Art Galery",
                 Organizor = "Masashi Kishimoto",
                 StartDate = new DateTime(2023, 10, 11),
                 EndDate = new DateTime(2023, 10, 27),
@@ -145,7 +140,7 @@ namespace EventApi.Data.Repository
             new Event()
             {
                 Id = 6,
-                Name = "Hamlet Tiyatrosu",
+                Name = "Hamlet Theatre",
                 Organizor = "10. Sokak Tiyatrosu",
                 StartDate = new DateTime(2023, 9, 20),
                 EndDate = new DateTime(2023, 10, 2),
@@ -155,7 +150,7 @@ namespace EventApi.Data.Repository
             new Event()
             {
                 Id = 7,
-                Name = "Fırtına Tiyatrosu",
+                Name = "Tempest Theatre",
                 Organizor = "İstanbul Belediyesi",
                 StartDate = new DateTime(2023, 8, 10),
                 EndDate = new DateTime(2023, 8, 21),
@@ -266,9 +261,129 @@ namespace EventApi.Data.Repository
                 IsStudent = true
             });
 
-            modelBuilder.Entity<Image>().HasData(new Image()
+            modelBuilder.Entity<Image>().HasData(
+            // CONCERT
+            new Image()
             {
-
+                Id = 1,
+                ImageUrl = "https://i2.sdacdn.com/haber/2022/09/10/izmir-de-2-milyon-insani-costuran-tarkan-dunya-15263959_8699_m.jpg",
+                EventId = 1
+            },
+            new Image()
+            {
+                Id = 2,
+                ImageUrl = "https://www.indyturk.com/sites/default/files/styles/1368x911/public/article/main_image/2022/09/10/1008296-2147193470.jpg?itok=Yj-6bOHU",
+                EventId = 1
+            },
+            new Image()
+            {
+                Id = 3,
+                ImageUrl = "https://i4.hurimg.com/i/hurriyet/75/0x0/6319fd174e3fe02f6483d47d.jpg",
+                EventId = 1
+            },
+            new Image()
+            {
+                Id = 4,
+                ImageUrl = "https://www.billboard.com/wp-content/uploads/media/EMA-2015-Jason-Derulo-Performance-BIllboard-650.jpg?w=650",
+                EventId = 2
+            },
+            new Image()
+            {
+                Id = 5,
+                ImageUrl = "https://www.regnumhotels.com/media/b4scwtmn/regnumcarya-jasenderulo-banner.jpg",
+                EventId = 2
+            },
+            new Image()
+            {
+                Id = 6,
+                ImageUrl = "https://i.ytimg.com/vi/U7y6257_0kQ/maxresdefault.jpg",
+                EventId = 2
+            },
+            new Image()
+            {
+                Id = 7,
+                ImageUrl = "https://www.bizimizmir.net/images_haberler/2018/10/31/haber-2018-10-30-1540931409354-sebnem.jpg",
+                EventId = 3
+            },
+            new Image()
+            {
+                Id = 8,
+                ImageUrl = "https://i2.milimaj.com/i/milliyet/75/0x0/6318c4ef86b24504a4ff1a93.jpg",
+                EventId = 3
+            },
+            new Image()
+            {
+                Id = 9,
+                ImageUrl = "https://www.denizliekspres.com.tr/images/haberler/2017/05/sebnem-ferah-konseri-iptal.jpg",
+                EventId = 3
+            },
+            // ART GALERY
+            new Image()
+            {
+                Id = 10,
+                ImageUrl = "https://bayaiyi.com/wp-content/uploads/2017/01/Mona-Lisa-Leonardo-da-Vinci-1.jpg",
+                EventId = 4
+            },
+            new Image()
+            {
+                Id = 11,
+                ImageUrl = "https://i4.hurimg.com/i/hurriyet/75/1200x675/612eda5f4e3fe00c40fa733d.jpg",
+                EventId = 4
+            },
+            new Image()
+            {
+                Id = 12,
+                ImageUrl = "https://wp.oggusto.com/wp-content/uploads/2022/07/leonardo-da-vinci-hayati-eserleri-ve-bilinmeyenleri.webp",
+                EventId = 4
+            },
+            new Image()
+            {
+                Id = 13,
+                ImageUrl = "https://70f186a60af817fe0731-09dac41207c435675bfd529a14211b5c.ssl.cf1.rackcdn.com/assets/attachments_p/000/071/598/size500_Rebecca_shieh_naruto_approval.jpg",
+                EventId = 5
+            },
+            new Image()
+            {
+                Id = 14,
+                ImageUrl = "https://www.gotokyo.org/en/spot/ev290/images/main.jpg",
+                EventId = 5
+            },
+            new Image()
+            {
+                Id = 14,
+                ImageUrl = "https://www.gotokyo.org/en/spot/ev290/images/main.jpg",
+                EventId = 5
+            },
+            // THEATRE
+            new Image()
+            {
+                Id = 15,
+                ImageUrl = "https://i.ytimg.com/vi/Oq5HKX1vicM/maxresdefault.jpg",
+                EventId = 6
+            },
+            new Image()
+            {
+                Id = 16,
+                ImageUrl = "https://media.americanshakespearecenter.com/app/uploads/2017/10/16191919/hamlet_2018_web1.jpg",
+                EventId = 6
+            },
+            new Image()
+            {
+                Id = 17,
+                ImageUrl = "https://mdtheatreguide.com/wp-content/uploads/2018/01/HAMLET2018_0073-e1517186502587-550x348.jpg",
+                EventId = 6
+            },
+            new Image()
+            {
+                Id = 18,
+                ImageUrl = "https://www.theoldglobe.org/link/beaa065ced414a8a8760da8c048cfbd8.aspx?id=30870",
+                EventId = 7
+            },
+            new Image()
+            {
+                Id = 19,
+                ImageUrl = "https://i8a4b7e5.stackpathcdn.com/wp-content/uploads/2017/07/the-tempest-topher-mcgrillis-1.jpg",
+                EventId = 7
             });
         }
 
